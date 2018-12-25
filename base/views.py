d7a7a1e36111f7601d4eb8b7fb2eb02aee80a94d
Jason Zou <shutupjz@gmail.com>
2018-12-24 19:27:42 -0800

    comment out google vision

--- base/views.py
+++ base/views.py
@@ -1,9 +1,9 @@
 # === Google Cloud Imports === #
-import io
-import os
+# import io
+# import os
 
-from google.cloud import vision
-from google.cloud.vision import types
+# from google.cloud import vision
+# from google.cloud.vision import types
 # ============================ #
 
 from django.shortcuts import render, redirect
